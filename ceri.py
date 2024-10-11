c86760f225c67e6ce9aab52a4c85e2ae923817d2
Tiqur <trevorbrage02@gmail.com>
2024-10-10 22:02:20 -0700

    Added grayscale and thresholding

--- ceri.py
+++ ceri.py
@@ -1,18 +1,38 @@
 import cv2
 
 class CERI:
+    # Debugging
+    image_index = 0
+
     def __init__(self):
         pass
 
-    def save_image(self, img, name):
+    def save_image(self, img, name=None):
+        if name is None:
+            name = self.image_index
+
         cv2.imwrite(f'{name}.png', img)
         print(f'Saved {name}.png')
+        self.image_index += 1
 
     def detect(self, image_path):
         self.image = cv2.imread(image_path)
-
-        if image is None:
+        if self.image is None:
             raise ValueError("Failed to load the image.")
 
-        self.save_image(self.image, "1")
+        self.identify_text_elements(10, 4)
+
+
+    def identify_text_elements(self, horizontal_threshold, vertical_threshold):
+        # Step 1: Convert to grayscale
+        grayscale = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self.save_image(grayscale)
+
+        # Step 2: Apply adaptive thresholding
+        thresh = cv2.adaptiveThreshold(grayscale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+        self.save_image(thresh)
+
+        # Step 3: Find contours in the thresholded image
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
 
